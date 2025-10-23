f9f7adc385285a0caf854b0fde788b79ed326243
Fandresena <fandresena.andriatsizaraina@kowee.fr>
2025-10-23 10:29:54 +0200

    first commit

---
+++ gereo/src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+function App() {
+
+  return (
+    <>
+     <h1 className='text-2xl text-sky-400 text-center mt-8'>Hello from GEREO</h1>
+    </>
+  )
+}
+
+export default App
